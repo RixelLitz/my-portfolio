a34fca21edd46400c21bb493f145f1b52fdfaeff
Rian PC2 <contatorianbritto@gmail.com>
2024-09-18 13:35:42 -0300

    feat: Added avatar and animation

--- src/pages/Home/Sections/Hero/Hero.tsx
+++ src/pages/Home/Sections/Hero/Hero.tsx
@@ -1,12 +1,21 @@
+import { AnimatedBackground } from "../../../../components/AnimatedBackground/AnimatedBackground";
+
 const Hero = () => {
-  return(
-      <>
-      <section className="h-screen">
-        <div className="bg-black w-full h-full">
-            <h1 className="text-gray-700 ">teste</h1>
+  return (
+    <section className="h-screen bg-neutral-900 flex items-center">
+                <AnimatedBackground/>
+      <div className="w-full h-1/2 flex justify-evenly items-center relative ">
+        <div className="relative  h-full">
+
+          <img className="relative rounded-full z-10 h-full" src="./src/assets/avatar.jpeg" alt="" />
         </div>
-      </section>
-      </>
-  )
-}
-export default Hero
+        <div className="text-white flex flex-col">
+          <h1 className="text-5xl">Rian Britto Pereira</h1>
+          <h2 className="text-3xl">FullStack Developer</h2>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
